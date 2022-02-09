ef8a176a8bfc25560b43abec77f90b2f3c9b85ba
Rutvik Bhimani <rutvik.b@simformsolutions.com>
2022-02-09 11:06:35 +0530

    add some more command

--- linuxbash/filesNdirectory/createdir.sh
+++ linuxbash/filesNdirectory/createdir.sh
@@ -1,36 +1,28 @@
 #! /bin/bash
 
 echo -e "\n Welcome to bash file created by rutvik\n\n";
+sleep 3
 
 echo  -e "Creating hellolinux,willdelete directory.. using mkdir command.. with rwx permission"
-
 mkdir hellolinux willdelete -v -m 777
-
 sleep 3
 
 echo  -e "\n lets see what we created...."
-
 ls 
-
 sleep 3
 
 echo  -e "\n  now after 3 secound willdelete is deleted"
-
 rmdir willdelete
+sleep 3
 
 echo  -e "\n lets see what left ...."
-
 ls 
-
 sleep 2
 
 cd hellolinux
-
-sleep 3
-
 echo  -e "\n Where are you ?? lets see using pwd command"
-
 pwd
+sleep 3
 
 echo  -e "\n Surprice we changed your directory using cd command"
 
--- linuxbash/filesNdirectory/files.sh
+++ linuxbash/filesNdirectory/files.sh
@@ -2,23 +2,31 @@
 
 echo -e "\n Welcome to bash file created by rutvik\n\n";
 sleep 3
-echo -e "Lets Create our first file first.txt using touch command"
-sleep 3
-touch first.txt
 
+echo -e "Lets Create our first file first.txt using touch command"
+touch first.txt
+sleep 3
 
 echo  -e "\n lets see what we created...."
-sleep 3
-
 ls 
+sleep 3
 
 echo  -e "\n now let copy default.txt content to copy.txt file"
-sleep 3
 cp -p default.txt copy.txt
-
 sleep 3
 
 echo -e "\n now lets see what we are actully just copy using cat command"
-
 cat copy.txt
+sleep 3
+
+
+echo -e "\n Lets move our newly genrated file to up one directory "
+mv copy.txt ../.
+sleep 3 
+
+echo -e "\n Lets give a nick name to first.txt --> secound.txt"
+mv first.txt secound.txt
+sleep 3
+
+echo -e "\n Thank You Hope you enjoyed the journy"
 
